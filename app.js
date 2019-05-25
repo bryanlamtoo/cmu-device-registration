6bc8613727583b1baf22be04c55a99b52b3ae5a0
Bryan Lamtoo <bryanlamtoo@gmail.com>
2019-05-25 16:52:01 +0000

    Changed to remote DB on atlas cluster

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+// var sassMiddleware = require('node-sass-middleware');
 var mongoose = require('mongoose');
 const cors = require('cors')
 
@@ -19,12 +19,12 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(sassMiddleware({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    indentedSyntax: true, // true = .sass and false = .scss
-    sourceMap: true
-}));
+// app.use(sassMiddleware({
+//     src: path.join(__dirname, 'public'),
+//     dest: path.join(__dirname, 'public'),
+//     indentedSyntax: true, // true = .sass and false = .scss
+//     sourceMap: true
+// }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/', indexRouter);
@@ -34,8 +34,18 @@ app.use('/api/v1/devices', devicesRouter);
 /************************************************
  *      DATABASE Connection                     *
  ************************************************/
-// mongoose.connect("mongodb+srv://esn_admin:esn_admin@esn-nn2md.mongodb.net/device_reg?retryWrites=true").then(result => {
-mongoose.connect("mongodb://localhost:27017/devices_reg");
+mongoose.connect("mongodb+srv://esn_admin:esn_admin@esn-nn2md.mongodb.net/device_reg?retryWrites=true",
+{
+    useNewUrlParser: true,
+    useCreateIndex: true
+  }, (err) => {
+    if (!err) {
+      console.log('Database connected successfully')
+    } else {
+      console.log('Error in database connection: ' + err)
+    }
+  })
+//mongoose.connect("mongodb://localhost:27017/devices_reg");
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'Connection error'));
 db.once('open', function () {
